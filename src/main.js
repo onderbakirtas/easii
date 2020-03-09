9a946dbde86573429cbb9277e720460e70ef4d94
Onder Bakirtas <onderbakirtas@gmail.com>
2020-03-10 02:07:19 +0300

    Added Element UI

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
 
 import 'bulma/bulma.sass'
 
 Vue.config.productionTip = false
 
+Vue.use(Element)
+
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
