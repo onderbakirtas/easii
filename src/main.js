5f76f6aab02b1da3d1d179d20737b9de8c5a794c
Onder Bakirtas <onderbakirtas@gmail.com>
2020-03-09 22:35:47 +0300

    Added Buefy and prettier config

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Buefy from 'buefy'
 
-Vue.config.productionTip = false;
+import 'buefy/dist/buefy.css'
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
